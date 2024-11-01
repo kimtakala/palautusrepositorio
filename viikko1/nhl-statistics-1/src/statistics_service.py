a18517ddb3770006a0ee04f0862345c7f7faf1a5
Kim Takala <kim.takala@helsinki.fi>
2024-11-01 15:15:58 +0200

    task 1

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,11 +1,7 @@
 from player_reader import PlayerReader
 
-
-
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
-
+    def __init__(self, reader: "PlayerReader"):
         self._players = reader.get_players()
 
     def search(self, name):
