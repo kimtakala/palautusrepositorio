d9039480876475ca6b38a46b5ff356af9ac44852
Kim Takala <kim.takala@helsinki.fi>
2024-11-05 16:40:31 +0200

    task 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,22 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license1, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license1
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _stringify_stuff(self, stuff):
+        return "\n".join(f'- {thing}' for thing in stuff) if stuff else "-"
+
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license}"
+            f"\n\nAuthors:\n{self._stringify_stuff(self.authors)}"
+            f"\n\nDependencies:\n{self._stringify_stuff(self.dependencies)}"
+            f"\n\nDevelopment dependencies:\n{self._stringify_stuff(self.dev_dependencies)}"
         )
