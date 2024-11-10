28a84d3d32a2db6dde6ada8631265782268bf20b
Kim Takala <kim.takala@helsinki.fi>
2024-11-10 19:42:59 +0200

    new shite

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -2,10 +2,14 @@
 Docstring: practice branching
 """
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
