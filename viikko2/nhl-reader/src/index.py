e267d859b0558064dec922ed5fb43cf8c50cca9a
Kim Takala <kim.takala@helsinki.fi>
2024-11-11 00:09:38 +0200

    part 11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,42 @@
+'importing the necessities'
+import requests
 from player import Player
 
 def main():
-    pass
+    'main function'
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    players = []
+    # "name":"Nils Lundkvist",
+    # "nationality":"SWE",
+    # "assists":17,
+    # "goals":2,
+    # "team":"DAL",
+    # "games":59,
+    # "id":8480878
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == 'FIN':
+            players.append(player)
+
+    print('Players from Finland')
+
+    for player in players:
+        print(f"""\
+ {player.name:<20}\
+ {player.team:<4}\
+ {player.goals:<2} +\
+ {player.assists:<2} =\
+ {player.goals + player.assists}\
+""")
 
 if __name__ == "__main__":
     main()
+
+    # Players from FIN
+
+    # Mikko Rantanen       COL  55 + 50 = 105
+    # Aleksander Barkov    FLA  23 + 55 = 78
+    # Roope Hintz          DAL  37 + 38 = 75
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,26 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    '''
+    A class for the NHL PLayers
+    dicti format:
+    {
+        "name":"Nils Lundkvist",
+        "nationality":"SWE",
+        "assists":17,
+        "goals":2,
+        "team":"DAL",
+        "games":59,
+        "id":8480878
+    }
+    '''
+    def __init__(self, dicti):
+        self.name = dicti['name']
+        self.nationality = dicti['nationality']
+        self.assists = dicti['assists']
+        self.goals = dicti['goals']
+        self.points = self.assists + self.goals
+        self.team = dicti['team']
+        self.games = dicti['games']
+        self.id = dicti['id']
+
     def __str__(self):
         return self.name
