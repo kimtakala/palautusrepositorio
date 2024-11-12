f24174dde86e52c0c76fb6fdad9c0cad8c1bb997
Kim Takala <kim.takala@helsinki.fi>
2024-11-12 16:38:31 +0200

    week 3 task 1

--- viikko3/hello-robot/src/CounterLibrary.py
+++ viikko3/hello-robot/src/CounterLibrary.py
@@ -1,18 +1,27 @@
+'module docstring'
 from counter import Counter
 
 
 class CounterLibrary:
+    'class substring'
     def __init__(self):
         self._counter = Counter()
 
     def increase_counter(self):
+        'info docstring'
         self._counter.increase()
 
     def increment_counter_by(self, amount):
+        'info docstring'
         int_amount = int(amount)
         self._counter.increment(int_amount)
 
     def counter_value_should_be(self, expected):
+        'info docstring'
         int_expected = int(expected)
         if self._counter.value != int_expected:
             raise AssertionError(f"{self._counter.value} != {int_expected}")
+
+    def reset_counter(self):
+        'info docstring'
+        self._counter = Counter()
--- viikko3/hello-robot/src/counter.py
+++ viikko3/hello-robot/src/counter.py
@@ -1,16 +1,22 @@
+'info docstring'
 class Counter:
+    'info docstring'
     def __init__(self, initial_value=0):
         self._initial_value = initial_value
         self.value = initial_value
 
     def increase(self):
+        'info docstring'
         self.value = self.value + 1
 
     def increment(self, amount):
+        'info docstring'
         self.value = self.value + amount
 
     def decrease(self):
+        'info docstring'
         self.value = self.value - 1
 
     def reset(self):
+        'info docstring'
         self.value = self._initial_value
