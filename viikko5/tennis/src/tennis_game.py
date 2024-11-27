700bc324967f4ab688ba854f6064bd1936ad04c1
Kim Takala <kim.takala@helsinki.fi>
2024-11-27 13:59:08 +0200

    done task 3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,56 @@
 class TennisGame:
-    def __init__(self, player1_name, player2_name):
+    def __init__(self, player1_name: str, player2_name: str):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.player1_score += 1
+        elif player_name == self.player2_name:
+            self.player2_score += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            raise KeyError("Incorrect player name!")
+    
+    def tie_game(self) -> str:
+        if self.player1_score == 0:
+            score = "Love-All"
+        elif self.player1_score == 1:
+            score = "Fifteen-All"
+        elif self.player1_score == 2:
+            score = "Thirty-All"
+        else:
+            score = "Deuce"
+        return score
+
+    def late_game(self) -> str:
+        player1_advantage = self.player1_score - self.player2_score
+        if player1_advantage == 1:
+            score = "Advantage player1"
+        elif player1_advantage == -1:
+            score = "Advantage player2"
+        elif player1_advantage >= 2:
+            score = "Win for player1"
+        else:
+            score = "Win for player2"
+        return score
+        
+    def mid_game(self):
+        scores = ["Love", "Fifteen", "Thirty", "Fourty"]
+        return scores[self.player1_score] + "-" + scores[self.player2_score]
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        # tie handler
+        if self.player1_score == self.player2_score:
+            score = self.tie_game()
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        # late game handler
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            score = self.late_game()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        # mid game handler
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            score = self.mid_game()
 
         return score
