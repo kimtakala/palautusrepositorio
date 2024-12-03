8f4ef211529977271e067730f8a68e99879a296f
Kim Takala <kim.takala@helsinki.fi>
2024-12-03 17:00:30 +0200

    task 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -13,9 +13,23 @@ def main():
         PlaysIn("PHI")
     )
 
+    
+    matcher = Or(
+    HasAtLeast(45, "goals"),
+    HasAtLeast(70, "assists")
+    )
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
