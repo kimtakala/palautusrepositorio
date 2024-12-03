2cf516391698c69317cca0193b1e0d9ba46eb6c2
Kim Takala <kim.takala@helsinki.fi>
2024-12-03 17:46:02 +0200

    Task 5

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,7 @@ class Or:
 
         return False
 
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
@@ -20,6 +21,7 @@ class And:
 
         return True
 
+
 class All:
     def __init__(self):
         pass
@@ -27,12 +29,14 @@ class All:
     def test(self, player):
         return True
 
+
 class Not:
     def __init__(self, matcher):
         self._matcher = matcher
-    
+
     def test(self, player):
         return not self._matcher.test(player)
+
 
 class PlaysIn:
     def __init__(self, team):
@@ -52,6 +56,7 @@ class HasAtLeast:
 
         return player_value >= self._value
 
+
 class HasFewerThan:
     def __init__(self, value, attr):
         self._value = value
@@ -61,3 +66,23 @@ class HasFewerThan:
         player_value = getattr(player, self._attr)
 
         return player_value < self._value
+
+
+class QueryBuilder:
+    def __init__(self, query=All()):
+        self.query_object = query
+
+    def plays_in(self, team):
+        return QueryBuilder(And(self.query_object, PlaysIn(team)))
+
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(self.query_object, HasAtLeast(value, attr)))
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(self.query_object, HasFewerThan(value, attr)))
+
+    def one_of(self, obj1, obj2):
+        return QueryBuilder(Or(obj1, obj2))
+
+    def build(self):
+        return self.query_object
